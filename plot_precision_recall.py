a933d287da2d0c477eeb8a6921690da40be72703
Garrett Wilson <g@floft.net>
2018-08-24 17:53:31 -0700

    A precision-recall plot per architecture type

--- plot_precision_recall.py
+++ plot_precision_recall.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import re
 import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -20,6 +21,38 @@ def loadPickle(filename):
 
     return data
 
+def getResults(folder):
+    pickle_match = re.compile("(.*).pickle")
+    results = {}
+    categories_first = None
+
+    # Find all files recursively in specified folder
+    for dirname, dirnames, filenames in os.walk(folder):
+        for filename in filenames:
+            # Get the number out of the filename indiciating number of training examples
+            m = pickle_match.match(filename)
+
+            if m is not None:
+                name = m.groups()[0]
+                data = loadPickle(os.path.join(dirname, filename))
+                results[name] = {
+                        'categories': data[0],
+                        'average_precision_per_class': data[1],
+                        'mean_ap': data[2],
+                        'precisions_per_class': data[3],
+                        'recalls_per_class': data[4],
+                        'corloc_per_class': data[5],
+                        'mean_corloc': data[6],
+                    }
+
+                # All should have the same categories
+                if categories_first is None:
+                    categories_first = data[0]
+                else:
+                    assert categories_first == data[0]
+
+    return results
+
 def plotCurve(title, ylabel, xlabel, curves, filename, loc='lower left'):
     """
     Title
@@ -38,25 +71,24 @@ def plotCurve(title, ylabel, xlabel, curves, filename, loc='lower left'):
     plt.legend(loc=loc)
 
     plt.savefig(os.path.join(config.datasetFolder, filename+".png"),
-            bbox_inches='tight', pad_inches=0, transparent=True)
+            bbox_inches='tight', pad_inches=0)#, transparent=True)
 
 if __name__ == "__main__":
-    filename = "precision-recall.pickle"
-    classes = ['dog', 'food', 'glass', 'keys', 'leash', 'pillbottle', 'plant', 'umbrella', 'watercan']
+    results = getResults(config.datasetFolder)
 
-    categories, average_precision_per_class, mean_ap, precisions_per_class, recalls_per_class, corloc_per_class, mean_corloc = loadPickle(filename)
-    assert classes==categories, "mismatching/unexpected classes: "+str(categories)
+    for key, values in results.items():
+        print("Architecture:", key)
+        curves = []
+        for i, cat in enumerate(values['categories']):
+            curves += [(cat['name'], values['recalls_per_class'][i], values['precisions_per_class'][i])]
 
-    curves = []
-    for i, classname in enumerate(classes):
-        curves += [(classname, recalls_per_class[i], precisions_per_class[i])]
+        plotColors = sns.color_palette()
+        #plotMarkers = ["s", "*", "x", "d", ".", "o", "v", "^", "<", ">", "1", "2", "3", "4" ]*5
+        #plotLines = ['-', '--', '-.', ':']*10
 
-    plotColors = sns.color_palette()
-    #plotMarkers = ["s", "*", "x", "d", ".", "o", "v", "^", "<", ">", "1", "2", "3", "4" ]*5
-    #plotLines = ['-', '--', '-.', ':']*10
+        plotCurve("Precision-Recall Curves by Class with "+key, "Precision", "Recall", curves, "precision_recall_"+key)
 
-    plotCurve("Precision-Recall Curves by Class", "Precision", "Recall", curves, "precision-recall")
-
-    print("Average precision per class (sanity check):")
-    for i, classname in enumerate(classes):
-        print(classname, "-", average_precision_per_class[i])
+        print("Average precision per class (sanity check):")
+        for i, cat in enumerate(values['categories']):
+            print(cat['name'], "-", values['average_precision_per_class'][i]*100, "%")
+        print()
